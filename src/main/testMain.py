254cd5e00c7148cfbfe9e7e17d4883551c3a7a88
kallieb <kallie.bracken@outlook.com>
2016-04-12 17:03:34 -0600

    Made separate mains

--- src/main/testMain.py
+++ src/main/testMain.py
@@ -11,6 +11,7 @@ import dataset
 import normalize
 import distance
 import regression
+import util
 
 
 ##############################################################
@@ -24,161 +25,29 @@ import regression
 # def experiment1(datasets):
 
 # ------------------------------------------------------------
-# PART 1: CHOOSING DATA
+# PART 1: ECTOR CONFIGURATION (CHOOSING COLUMNS) 
 # ------------------------------------------------------------
-	###############---VECTOR CONFIGURATION---################
-
-	# < Put work here -- this is where we choose columns>
-
-	###############---REDUCTION WITH REGRESSION---################
-
-	# < Put work here -- if desired, use regression to reduce dimensionality/columns to uniform k>
+	# "Manually" configure, or configure with regression:
+	# < Put work here -- if desired, use regression to reduce 
+	# dimensionality/columns to uniform k>
 
 # ------------------------------------------------------------
-# PART 2: NORMALIZATION AND CHOOSING DISTANCE MEASURE 
+# PART 2: CHOOSE NORMALIZATION AND DISTANCE FUNCTIONS
 # ------------------------------------------------------------
-
-	###############---VECTOR NORMALIZATION---################
-
 	# < Put work here >
 
 # ------------------------------------------------------------
-# PART 3: RUN
+# PART 3: RUN -- CHOOSE CLUSTERING ALGORITHM
 # ------------------------------------------------------------
-
-	###################---CLUSTERING---#####################
-
 	# < Put work here >
 
 # ------------------------------------------------------------
 # PART 4: WRITE RESULTS 
 # ------------------------------------------------------------
-	##################---STORE RESULTS---####################
-
-	# Prepare to write experiment file -- fill in the below values for this experiment.
-
-	# clusteringAlgorithmInfo = "gonzalez"
-	# distanceMeasurementInfo = "euclidean"
-	# vectorConfigurationInfo = "explicitly configured, same columns used across datasets, Indices used: [0,1,2,3]"
-
-	# writeFile(1, numClusters, clusteringAlgorithmInfo, distanceMeasurementInfo,vectorConfigurationInfo, clusters)
-
+	# Use WriteFile function--- BE THOROUGH FOR GOODNESS SAKE!
 
 ##############################################################
 ##############################################################
-
-
-# Iterates through datasets, gets the largest possible dimensionality for 
-# each dataset, then returns the minimum "maximum dimensionality." Use this 
-# function to know the possible range of dimensionalities that are valad for a
-# given group of datasets.
-def getMinDimensionalityAcrossDatasets(datasets):
-	minDimensionality = float('inf') 
-	for d in datasets:
-		tempMax = d.getMaximumDimensionality()
-		if  tempMax < minDimensionality:
-			minDimensionality = tempMax
-	return minDimensionality
-
-# Gets a list of <dimensionality> different random numbers, in the range
-# 0-<dataMaxDimensionality>.
-# Use this function to randomly generate an array of active columns
-def getListOfRandomIndices(dimensionality, dataMaxDimensionality):
-	activeVectorsArray = []
-	for i in range(0, dimensionality):
-		r = random.randint(0, dataMaxDimensionality-1)
-		while r in activeVectorsArray:
-			r = random.randint(0, dataMaxDimensionality-1)
-		activeVectorsArray.append(r)
-	return activeVectorsArray
-
-
-# Arguments:
-#	-datasets: List of dataset objects, for each of which a dictionary of labelled vectors will be returned
-# 	-dimensionality: The number of columns/components to be used to form each vector
-# 	-sameColumnsForAllDatasets: If true, the same set of random indices is used for each dataset. If false, a different set of random indices is used to create vectors for each dataset.
-# Returns:
-#	-A list of dictionaries (labell---> vector)
-def randomlyConfigureActiveColumns(datasets, dimensionality, sameColumnsForAllDatasets):
-	
-	dataDictionaries = []
-
-	minDimensionality = getMinDimensionalityAcrossDatasets(datasets)
-	if dimensionality > minDimensionality: # Ensure dimensionality is suitable for all datasets.
-		print "ERROR 1:", dimensionality, "-dimensional vectors cannot be generated for the datasets you are currently using because one or more of the datasets does not have enough data/columns."
-
-	activeColumns = False
-	if sameColumnsForAllDatasets:
-		activeColumns = getListOfRandomIndices(dimensionality, minDimensionality)
-
-	for d in datasets:
-		if sameColumnsForAllDatasets:
-			dataDictionaries.append(d.getVectorsByArray(activeColumns))
-		else:
-			dataDictionaries.append(d.getRandomVectorsByDimensionality(dimensionality))
-
-	return dataDictionaries
-
-# Arguments:
-#	-list of datasets
-# 	-list of indice arrays--an array for each dataset.
-#	-If sameArrayForAll is true, use the same list of indices for all datasets. In this case, the second argument is a single list of indices.
-def explicitlyConfigureActiveColumns(datasets, listOfActiveIndices, sameArrayForAll):
-	
-	dataDictionaries = []
-
-	if sameArrayForAll:
-		activeColumns = listOfActiveIndices
-		for d in datasets:
-			dataDictionaries.append(d.getVectorsByArray(activeColumns))
-	else:
-		for i in range(0, len(listOfActiveIndices)):
-			dataDictionaries.append(datasets[i].getVectorsByArray(listOfActiveIndices[i]))
-
-	return dataDictionaries
-
-
-# Takes a list of dictionaries and returns a single dictionary
-
-def crunchDictionaryList(dictionaries):
-	bigDictionary = {}
-	for d in dictionaries:
-		items = d.items()
-		for i in items:
-			bigDictionary[i[0]] = i[1]
-	return bigDictionary
-
-
-def writeFile(expIndex, numClusters, clusteringAlgorithmInfo, distanceMeasurementInfo, vectorConfigurationInfo, otherNotes, clusters):
-
-	now = datetime.datetime.now()
-	month = now.month
-	day = now.day
-	hour = now.hour
-	minute = now.minute
-
-	filename = '../../results/exp'+str(expIndex)+"/"+str(expIndex)+'_'+str(month)+'.'+str(day)+'_'+str(hour)+'.'+str(minute)+'_K.'+str(numClusters)
-	f = open(filename,'w')
-
-	f.write("CONFIGURATION DETAILS:\n\n")
-	f.write("Experiment index: "+str(expIndex)+"\n")
-	f.write("Number of clusters: "+str(numClusters)+"\n\n")
-	f.write("Clustering algorithm: "+clusteringAlgorithmInfo+"\n")
-	f.write("Distance measurement: "+distanceMeasurementInfo+"\n")
-	print vectorConfigurationInfo
-	f.write("Vector configuration: "+vectorConfigurationInfo+"\n")
-	f.write("Notes: "+otherNotes+"\n")
-
-	for i in range(0, numClusters):
-		# print "Cluster: ", i
-		f.write("\nCLUSTER "+str(i)+":\n\n")
-		for key, val in clusters.iteritems():
-			if val==i:
-				f.write("\t"+key)
-
-	f.close()
-
-
 
 
 def experiment1(datasets, numClusters):
@@ -260,13 +129,13 @@ def experiment2(datasets, numClusters, dimensionality):
 	writeFile(2, numClusters, clusteringAlgorithmInfo, distanceMeasurementInfo,vectorConfigurationInfo, "Trying to get this thing to work!",clusterResults[1])
 
 
-
+# Automatically generates results for every N
 # datasets: Sets to be clustered 
 # dimensionality: If >0, then set to this value, if <=0 set to maximum valid dimensionality for  this data.
-def experiment3(datasets, dimensionality):
+def experiment3(datasets, numClusters):
 
 	# ------------------------------------------------------------
-	# PART 1: CHOOSING DATA
+	# PART 1: CHOOSING DATA (columns)
 	# ------------------------------------------------------------
 	###############---VECTOR CONFIGURATION---################
 	###############---REDUCTION WITH REGRESSION---################=
@@ -291,7 +160,6 @@ def experiment3(datasets, dimensionality):
 	# ------------------------------------------------------------
 	# PART 3: RUN
 	# ------------------------------------------------------------
-
 	###################---CLUSTERING---#####################
 	clusterResults  = cluster.gonzalez(crunchDictionaryList(normalizedDictionaries), numClusters, distance.euclidean);
 
@@ -310,18 +178,20 @@ def experiment3(datasets, dimensionality):
 
 
 if __name__ == "__main__":
-	# Read data files.
-	reader = read.Read(False) # READS EVERYTHING
+
+	#-----------------------------------
+	# Read ALL data files.
+	reader = read.Read(False) # READS EVERYTHING, and makes dataset objects from each T#.txt file
 	# Get data as list of dataset objects.
-	datasets = reader.getDataSets() # EVERY DATASET IS NOW AT YOUR DISPOSAL
+	datasets = reader.getDataSets() # EVERY DATASET IS NOW AT YOUR DISPOSAL, and indices match dataset titles.
+	#-----------------------------------
 
-
-	copy1 = list(datasets)
+	copy1 = list(datasets) # This is where you choose which tables you're using
 	# copy1.pop(0)
-	experiment1(copy1, 3)
+	experiment1(copy1, 3) # This is where you choose which tables you're using
 
-	copy2 = list(datasets)
+	copy2 = list(datasets) # This is where you choose which tables you're using
 	# copy2.pop(0)
-	experiment2(copy2, 3, 2)
+	experiment2([copy2[2]], 3, 2) # This is where you choose which tables you're using
 
 
