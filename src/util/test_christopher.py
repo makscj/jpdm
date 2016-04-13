812c0355a112dd16837507d33f47bd209ab22717
maksus01 <maks.cegielski@gmail.com>
2016-04-12 18:10:05 -0600

    adding test

--- src/util/test_christopher.py
+++ src/util/test_christopher.py
@@ -3,6 +3,8 @@
 
 import normalize
 
+
+print   "Maksnormalize", normalize.normalize({"A": [1,1,1,1], "B": [0,2,0,2]})
 print   "normalize", normalize.normalize({"something": [.001,123.123,54,3,4,100,1,13], "somethingElse": [1,1,1,1,1,1,1,1]})
 print "normalizeMinMax", normalize.normalizeMinMax({"something": [.001,123.123,54,3,4,100,1,13], "somethingElse": [1,1,1,1,1,1,1,1]})
 print "normalizeStandardize", normalize.normalizeStandardize({"something": [.001,123.123,54,3,4,100,1,13], "somethingElse": [1,1,1,1,1,1,1,1]})
